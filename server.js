2f1845dc03b4f55f7d11d4f2066d4641cebdd5a6
StephanieWgs <126796594+StephanieWgs@users.noreply.github.com>
2025-02-22 14:22:18 +0700

    Router & Entry Point

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 //Entry point backend
 
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+require('dotenv').config();
+
 const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Routers
+app.use("/api/auth", authRoutes);
+app.use("/api", taskRoutes);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
