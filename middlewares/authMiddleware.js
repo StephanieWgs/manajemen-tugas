9da42393aafcb94b76b083702b0448f87d438148
StephanieWgs <126796594+StephanieWgs@users.noreply.github.com>
2025-02-22 14:08:22 +0700

    Middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+// Middleware untuk Authentication
+
+const { verifyToken } = require("../config/auth");
+
+const authMiddleware = (req, res, next) => {
+    const token = req.header("Authorization")?.replace("Bearer ", "");
+    if (!token) {
+        return res.status(401).json({ message: "Akses Ditolak" });
+    }
+
+    try {
+        const decoded = verifyToken(token);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Invalid Token" });
+    }
+}
+
+module.exports = authMiddleware;
